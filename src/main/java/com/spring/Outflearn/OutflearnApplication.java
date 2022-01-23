731928611acb9ac2715472742ea4bdf2796f548b
jaeyeon <jae518@naver.com>
2022-01-23 18:48:34 +0900

    Initial commit

---
+++ src/main/java/com/spring/Outflearn/OutflearnApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Outflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutflearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutflearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/Outflearn/OutflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Outflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutflearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
