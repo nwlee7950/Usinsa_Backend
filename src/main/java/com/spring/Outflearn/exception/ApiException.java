1a078de09e18feccdec7d1b41a27bb0abe4f5553
jaeyeon <jae518@naver.com>
2022-01-23 19:33:10 +0900

    예외 처리

---
+++ src/main/java/com/spring/Outflearn/exception/ApiException.java
@@ -0,0 +1,35 @@
+package com.spring.Outflearn.exception;
+
+import lombok.Getter;
+
+public class ApiException extends RuntimeException{
+    @Getter
+    private ApiErrorCode errorCode;
+
+    public ApiException(String message, ApiErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public ApiException(ApiErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ApiException(ApiErrorCode errorCode, Throwable t) {
+        super(errorCode.getMessage(), t);
+    }
+
+
+    public ApiException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public ApiException(String msg) {
+        super(msg);
+    }
+
+    public ApiException() {
+        super();
+    }
+}
