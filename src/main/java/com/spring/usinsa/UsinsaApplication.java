93515dbd200900f2d728df4ccfd55badb19fd298
jaeyeon <jae518@naver.com>
2022-02-10 11:55:57 +0900

    OAuth 통신을 위한 RestTemplate Bean 등록

--- src/main/java/com/spring/usinsa/UsinsaApplication.java
+++ src/main/java/com/spring/usinsa/UsinsaApplication.java
@@ -3,7 +3,9 @@ package com.spring.usinsa;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -13,4 +15,9 @@ public class UsinsaApplication {
 		SpringApplication.run(UsinsaApplication.class, args);
 	}
 
+	// OAuth 와의 통신을 위한 RestTemplate Bean
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
