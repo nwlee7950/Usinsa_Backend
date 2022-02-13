5cdcd7319384988ae2421bae8fffbd18cfd6c25e
jaeyeon <jae518@naver.com>
2022-02-14 01:53:07 +0900

    로그인 테스트를 위한 UserLoginRequestDto @Builder 추가

--- src/test/java/com/spring/usinsa/repository/UserRepositoryTest.java
+++ src/test/java/com/spring/usinsa/repository/UserRepositoryTest.java
@@ -6,19 +6,27 @@ import com.spring.usinsa.model.Role;
 import com.spring.usinsa.model.Social;
 import com.spring.usinsa.model.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 //@RunWith(SpringRunner.class)  // JUnit4 일때 사용
+@ExtendWith(MockitoExtension.class)
 @DataJpaTest    // JPA Repository 들에 대한 빈들을 등록 -> EmbeddedDatabase 를 사용해버리기 때문에, mysql 설정 불가.
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)    // (replace = Replace.NONE)를 통해서 TestDatabaseAutoConfiguration 에서 DataSource 가 bean 으로 등록되지 않게 하면 DataSourceAutoConfiguration 에 의해서 DataSource 가 등록되게 된다.
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Spy
+    private BCryptPasswordEncoder passwordEncoder;
 
     private static final String username = "jae518";
     private static final String password = "wodus123";
@@ -32,7 +40,7 @@ public class UserRepositoryTest {
         // given
         User user = User.builder()
                 .username(username)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .name(name)
                 .role(Role.ROLE_USER)
                 .nickname(nickname)
